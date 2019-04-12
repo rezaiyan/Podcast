90d97ba5d91e20d3ef6ba9b994ace0ca806e462f
rezaiyan <alirezaiyann@gmail.com>
2019-04-12 15:03:14 +0430

    Test module created

---
+++ sdk-test/src/main/java/com/hezaro/wall/sdk/test/AcceptanceTest.kt
@@ -0,0 +1,22 @@
+package com.hezaro.wall.sdk.test
+
+import android.app.Activity
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import org.hamcrest.Matchers
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+abstract class AcceptanceTest<T : Activity>(clazz: Class<T>) {
+
+    @Rule @JvmField
+    val testRule: ActivityTestRule<T> = IntentsTestRule(clazz)
+
+    val checkThat: Matchers = Matchers()
+    val events: Events = Events()
+}
+

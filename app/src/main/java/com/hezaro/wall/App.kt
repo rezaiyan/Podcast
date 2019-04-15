1272501cd8cd02d8c8739608323f0a244c62fef0
rezaiyan <alirezaiyann@gmail.com>
2019-04-16 00:10:56 +0430

    Timber logger added

--- app/src/main/java/com/hezaro/wall/App.kt
+++ app/src/main/java/com/hezaro/wall/App.kt
@@ -3,10 +3,14 @@ package com.hezaro.wall
 import android.app.Application
 import com.hezaro.wall.utils.module
 import org.koin.android.ext.android.startKoin
+import timber.log.Timber
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin(this, listOf(module))
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }

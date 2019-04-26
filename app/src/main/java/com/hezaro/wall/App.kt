6d3b90b2a9e11af3fe4312e54b9cbbde71aa2026
rezaiyan <alirezaiyann@gmail.com>
2019-04-27 00:06:05 +0430

    Farsi font added

--- app/src/main/java/com/hezaro/wall/App.kt
+++ app/src/main/java/com/hezaro/wall/App.kt
@@ -11,14 +11,10 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin(this, listOf(module))
+
         if (BuildConfig.DEBUG) {
             Logger.addLogAdapter(AndroidLogAdapter())
             Timber.plant(Timber.DebugTree())
-            //            Timber.plant(object : Timber.DebugTree() {
-//                override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-//                    Logger.log(priority, tag, message, t)
-//                }
-//            })
         }
     }
 }
